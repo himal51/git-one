e4ac7830b30a3a3448a8fc2c2b901db0c167b27a
Himal Majumder <himalmajumder@gmail.com>
2020-08-19 16:52:54 +0600

    Add Index.html file body and css stylesheet

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$data = [
+    'key1'  => 'Value',
+    'key2'  => 'Value2',
+    'key21' => 'Value21',
+];
+
+function say( $data ) {
+    printf( "Current Value =%s\n", $data );
+}
+
+ 
---
+++ main.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    var himal = "";
+    alert(1);
+}
